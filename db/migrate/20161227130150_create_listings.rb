b7df88471cc1bd02960fa0e318c53957c0ff6329
hiromaki <h.makikawa@gmail.com>
2016-12-31 19:40:45 +0900

    first commit

---
+++ db/migrate/20161227130150_create_listings.rb
@@ -0,0 +1,19 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+      t.string :home_type
+      t.string :pet_type
+      t.string :pet_size
+      t.integer :breeding_years
+      t.string :address
+      t.string :listing_title
+      t.text :listing_content
+      t.integer :price_pernight
+      t.string :active
+      t.string :boolean
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
